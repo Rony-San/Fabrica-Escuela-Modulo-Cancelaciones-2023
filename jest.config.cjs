da336b606f7f89b965829a330acf6b95379e02d7
Jeison Moncayo <desarrollo5ude@udea.edu.co>
2023-04-20 17:36:51 -0500

    Tests

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,13 +1,4 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFiles: ['./jest.setup.js'],
-  collectCoverage: true,
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx}',
-    '!src/*.{js,jsx}',
-    '!src/app/**/*',
-    '!src/assets/**/*',
-    '!src/routes/**/*',
-  ],
-  coverageReporters: ['json', 'lcov', 'text', 'clover'],
+  setupFiles: ['./jest.setup.js']
 }
