75ec43c91280e4bd083224e21feb26f4caeb976f
Jaime Muñoz <jaime.munozq@udea.edu.co>
2023-04-21 11:41:14 -0500

    Agregar funcionalidad al login

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,14 +1,17 @@
 import FormLogin from '../../components/login/FormLogin'
+import { ToastContainer } from 'react-toastify'
 //import backgroundLogin from '../../assets/images/backgroundLogin.jpg'
 import './Login.css'
 
 export default function Login() {
   return (
-    <div class='center'>
-      <div class='image'>
-        <img src='/public/backgroundLogin.jpg' alt='' />
+    <div className='center'>
+      <div className='image'>
+        <img src='/public/backgroundLogin.jpg' alt='udea' />
       </div>
       <FormLogin />
+      <ToastContainer position="top-left" autoClose={4000} />
     </div>
+    
   )
 }
